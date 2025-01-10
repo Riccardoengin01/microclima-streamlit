bd081f173172a6b9fffdb020a259d395997ba12d
Riccardoengin01 <riccardoengin@gmail.com>
2025-01-10 20:53:34 +0100

    pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,3 +1,10 @@
+# Questo file fa parte del progetto Microclima-Streamlit.
+#
+# Microclima-Streamlit è distribuito sotto la licenza GNU General Public License v3.0.
+# Puoi utilizzare, modificare e distribuire questo software secondo i termini della licenza.
+#
+# Per maggiori dettagli, consulta il file LICENSE o visita https://www.gnu.org/licenses/gpl-3.0.html.
+
 from fpdf import FPDF
 import tempfile
 import matplotlib.pyplot as plt
