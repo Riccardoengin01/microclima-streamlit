72bb1b0b49555c77a625600ae992e9cbd6642c19
Riccardoengin01 <riccardoengin@gmail.com>
2025-06-08 10:39:47 +0200

    Aggiunge web app Flask e struttura static

---
+++ modules/layout.py
@@ -0,0 +1,7 @@
+# Questo file fa parte del progetto Microclima-Streamlit.
+#
+# Microclima-Streamlit è distribuito sotto la licenza GNU General Public License v3.0.
+# Puoi utilizzare, modificare e distribuire questo software secondo i termini della licenza.
+# Per maggiori dettagli, consulta il file LICENSE o visita https://www.gnu.org/licenses/gpl-3.0.html.
+"""Wrapper per importare le funzioni di layout."""
+from ..layout import *  # noqa: F401,F403
